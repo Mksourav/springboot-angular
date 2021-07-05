3a65a24ba8eb627c9d45c789a56a37494f539704
MRITUNJAY KUMAR SOURAV <mkignito@gmail.com>
2021-07-05 11:09:46 +0530

    Update dbdump.sh

--- dbdump.sh
+++ dbdump.sh
@@ -3,5 +3,5 @@ echo *** Backup Started on `date` ***
 timestamp=`date +%Y%m%d_%H%M`
 
 echo Creating backup file mydb.sql
-mysqldump -u demo --password="demo" --routines mydb > /home/lakj-backups/mydb-$timestamp.sql
+mysqldump -u demo --password="demo" --routines mydb > /home/mysql-backups/mydb-$timestamp.sql
 echo Finished creating backup file.
